ecfbd6cd9e876710814f827f2b7c7e573c7f8bbb
Matthew Moyer <matthewrmoyer@gmail.com>
2018-01-17 13:14:11 -0500

    run all migrations

--- migrations/20180117123114_board.js
+++ migrations/20180117123114_board.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('board', table => {
-    table.increments('id').primary();
+    table.increments("id").primary();
     table.string("name").notNullable()
     table.dateTime("created_at").notNullable().defaultTo(knex.fn.now())
-    table.string("creator").index().references("id").inTable("user").onDelete("cascade").notNull()
+    table.integer("creator").index().references("id").inTable("user").onDelete("cascade").notNull()
   })
 }
 
