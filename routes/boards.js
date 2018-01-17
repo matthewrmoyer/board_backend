70d5e73824c759526230d340f11f3070c8ab9406
Unknown <matthewrmoyer@gmail.com>
2018-01-17 17:46:28 -0500

    async await

--- routes/boards.js
+++ routes/boards.js
@@ -9,4 +9,32 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', async (req, res, next) => {
+    const boardId = req.params.id
+    try {
+        const data = await getById('board', boardId)
+        res.send(data)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/singleboard/:id', async (req, res, next) => {
+    const boardId = req.params.id
+    try {
+        const data = {}
+        const board = await getByTableColumn('board', boardId)
+        const creator = await getByTableColumn('users', board[0].creator)
+
+        data.board = board[0]
+        data.creator = creator[0]
+        res.send(data)
+    } catch(err) {
+        next(err)
+    }
+})
+
+function getByTableColumn(table, column) {
+    return knex(table).where('id', column)
+}
 module.exports = router;
